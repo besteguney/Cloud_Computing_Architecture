70bedac66b32b575f04d45941cc89dc84d0f17fe
besteguney <63497613+besteguney@users.noreply.github.com>
2024-03-13 15:07:12 +0100

    changes

--- client_measure.sh
+++ client_measure.sh
@@ -5,35 +5,8 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-# Creating the inference
-if [ $3 -eq 1]; then
-    kubectl create -f interference/ibench-cpu.yaml
-elif [ $3 eq 2]; then
-    kubectl create -f interference/ibench-l1d.yaml
-elif [ $3 eq 3]; then
-    kubectl create -f interference/ibench-l1i.yaml
-elif [ $3 eq 4]; then
-    kubectl create -f interference/ibench-l2.yaml
-elif [ $3 eq 5]; then
-    kubectl create -f interference/ibench-llc.yaml
-elif [ $3 eq 6]; then
-    kubectl create -f interference/ibench-membw.yaml
-
+current_time=$(date +"%T")
 ./mcperf -s $1 --loadonly
 ./mcperf -s $1 -a $2 \
     --noload -T 16 -C 4 -D 4 -Q 1000 -c 4 -t 5 -w 2\
-    --scan 5000:55000:5000 > results.txt
-
-# Deleting the inference pod
-if [ $3 -eq 1]; then
-    kubectl delete pods ibench-cpu
-elif [ $3 eq 2]; then
-    kubectl delete pods ibench-l1d
-elif [ $3 eq 3]; then
-    kubectl delete pods ibench-l1i
-elif [ $3 eq 4]; then
-    kubectl delete pods ibench-l2.yaml
-elif [ $3 eq 5]; then
-    kubectl delete pods ibench-llc.yaml
-elif [ $3 eq 6]; then
-    kubectl delete pods ibench-membw.yaml
+    --scan 5000:55000:5000 > "output_$3_$current_time.txt"
--- launch_memcached.sh
+++ launch_memcached.sh
@@ -3,3 +3,4 @@ kubectl expose pod some-memcached --name some-memcached-11211  \
                                     --type LoadBalancer --port 11211 \
                                     --protocol TCP
 sleep 60
+kubectl get service some-memcached-11211
