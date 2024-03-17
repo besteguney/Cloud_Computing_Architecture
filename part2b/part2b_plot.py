6dfe22cd40996bb7dd84a9f2244d4330c092f367
Tunaberk Almaci <almacitunaberk@gmail.com>
2024-03-17 18:52:38 +0100

    Part 2 plots changed

--- part2b/part2b_plot.py
+++ part2b/part2b_plot.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 
 DIR = "./part2b/data.csv"
 WORKLOADS = ["blackscholes", "canneal", "dedup", "ferret", "freqmine", "radix", "vips"]
@@ -8,9 +9,10 @@ MARKERS = ["o", "v", "s", "*", "x", "d", "P"]
 THREADS = [1, 2, 4, 8]
 fig = plt.figure(figsize=(14, 10))
 ax = fig.gca()
+ax.set_title("Normalized speedup by number of threads", pad=16, fontsize=20)
 
 # Linear Speedup Line
-ax.errorbar(x=range(0, 9),y=range(0, 9),label="Linear Speedup",linestyle="--",marker=".", markersize=8, capsize=2)
+ax.errorbar(x=range(0, 9),y=range(0, 9),label="Linear speedup",linestyle="--",marker=".", markersize=8, capsize=2)
 df = pd.read_csv(DIR, sep=";", header=0)
 for marker_idx, workload in enumerate(WORKLOADS):
     curr_df = df[df["workload"] == workload]
@@ -20,15 +22,15 @@ for marker_idx, workload in enumerate(WORKLOADS):
     ax.errorbar(x=THREADS, y=y_values, label=workload, marker=MARKERS[marker_idx], markersize=8, capsize=4)
 
 
-ax.set_xlabel("Number of Threads")
-ax.set_ylabel("Normalized Speedup")
+ax.set_xlabel("Number of threads", labelpad=16, fontsize=16)
+ax.set_ylabel("Normalized speedup", labelpad=16, fontsize=16)
 ax.legend(loc="upper right", fontsize=14)
 ax.grid(True, color="lightgray", linestyle="--", linewidth=1)
-ax.tick_params(labelsize=12)
+ax.tick_params(labelsize=18)
 ax.set_xlim(left=0, right=9)
 ax.set_ylim(bottom=0, top=9)
 ax.set_yticks(range(0, 9, 1))
 ax.set_xticks([1, 2, 4, 8])
 if not os.path.exists("./part2b/plots/"):
     os.makedirs("./part2b/plots")
-plt.savefig("./part2b/plots/part2b.png")
+plt.savefig("./part2b/plots/part2b.pdf")
