8dfff2a106d8b31ee98b2c01867cceab5eac74db
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-04-27 12:12:45 +0200

    Checkpoint

---
+++ part3/run.py
@@ -0,0 +1,126 @@
+import datetime
+import logging
+import re
+import subprocess
+import time
+
+
+MEMCACHED = "memcached"
+BLACKSCHOLES = "parsec-blackscholes"
+CANNEAL = "parsec-canneal"
+DEDUP = "parsec-dedup"
+FERRET = "parsec-ferret"
+RADIX = "parsec-radix"
+VIPS = "parsec-vips"
+FREQMINE = "parsec-freqmine"
+
+ALL_JOBS = [BLACKSCHOLES, CANNEAL, DEDUP, FERRET, RADIX, VIPS, FREQMINE]
+
+def get_yaml(job):
+    return job + ".yaml"
+
+RUN = 1
+RUN_DIR = f"RUN{RUN}"
+LOG_FILE = f"{RUN_DIR}/stdout.log"
+
+open(LOG_FILE, "w").close()
+logging.basicConfig(filename=LOG_FILE, encoding='utf-8', level=logging.DEBUG)
+
+def info(msg):
+    print(f"INFO: {msg}")
+
+# self.node_a_2 = [
+#     [BLACKSCHOLES, RADIX]
+# ]
+
+# self.node_e_2 = [
+#     [FREQMINE],
+#     [FERRET, VIPS]
+# ]
+
+# self.node_b_4 = [
+#     [CANNEAL, DEDUP]
+# ]
+
+def run(cmd, log=True):
+    out = subprocess.run(cmd, capture_output=True)
+    stdout = out.stdout.decode('unicode_escape').strip()
+    stderr = out.stderr.decode('unicode_escape').strip()
+    if log and out.stdout:
+        logging.info(f"CMD OUT: {stdout}")
+
+    if log and out.stderr:
+        logging.error(f"CMD ERR: {stderr}")
+
+    return stdout
+
+def create_job(job):
+    info(f"Creating job {job}")
+    run(["kubectl", "create", "-f", get_yaml(job)])
+
+def clear():
+    run(["kubectl", "delete", "jobs", "--all"])
+
+class JobScheduler:
+    def __init__(self) -> None:
+        self.finished_jobs = []
+        self.iter_counter = 0
+        create_job(BLACKSCHOLES)
+        create_job(FREQMINE)
+        create_job(FERRET)
+        create_job(CANNEAL)
+
+    def finish_job(self, job):
+        print(f"Job {job} finished.")
+        self.finished_jobs.append(job)
+        if job == BLACKSCHOLES:
+            create_job(RADIX)
+        elif job == FERRET:
+            create_job(VIPS)
+        elif job == CANNEAL:
+            create_job(DEDUP)
+
+    def check(self):
+        if self.iter_counter % 15 == 0:
+            # Just to get some info in the log
+            run(["kubectl", "get", "jobs"])
+            run(["kubectl", "get", "pods", "-o", "wide"])
+
+        pod_stats = get_pod_stats()
+        is_finished = True
+
+        for (job, status) in pod_stats:
+            if status == "Succeeded": 
+                if job not in self.finished_jobs:
+                    self.finish_job(job)
+            else:
+                is_finished = False
+        
+        return is_finished
+
+
+def get_pod_stats():
+    pod_stats = {}
+    out = run(["kubectl", "get", "pods", "-o", "wide"], log=False)
+
+    for job in ALL_JOBS:
+        match = re.findall(re.compile(job + r"[a-zA-Z-]+\s+\d+\/\d+\s+(\w+)"), out)
+
+        if len(match) > 0:
+            pod_stats[job] = match[0]
+        else:
+            pod_stats[job] = "Not started"
+    
+    return pod_stats    
+
+start = datetime.datetime.now()
+info(f"Starting at {start}")
+
+job_scheduler = JobScheduler()
+while not job_scheduler.check():
+    time.sleep(1)
+
+end = datetime.datetime.now()
+info(f"Finished at {end} with duration {end - start}")
+
+# clear()
