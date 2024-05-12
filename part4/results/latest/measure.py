96749de330c5d823ab603c55b76654cf82020c2f
Tunaberk Almaci <almacitunaberk@gmail.com>
2024-05-12 15:00:48 +0200

    Part 4 Question 1D measure changed

--- part4/results/latest/measure.py
+++ part4/results/latest/measure.py
@@ -1,11 +1,12 @@
 import psutil
 import time
+from datetime import datetime
 
 # Function to measure CPU usage
 def measure_cpu_usage(interval=1, duration=5, num_cores=2):
     cpu_percentages = []
     start_time = time.time()
-    
+
     while time.time() - start_time <= duration:
         cpu_percent = psutil.cpu_percent(interval=interval, percpu=True)
         current_usage = 0
@@ -15,18 +16,18 @@ def measure_cpu_usage(interval=1, duration=5, num_cores=2):
             current_usage = cpu_percent[0]
         cpu_percentages.append(current_usage)
         time.sleep(interval)
-    
+
     average_cpu_usage = sum(cpu_percentages) / len(cpu_percentages)
     return average_cpu_usage
 
 # Main function
 def main():
-    interval = 1  
-    duration = 5 
-    
+    psutil.cpu_percent(interval=None, percpu=True)
     while True:
-        average_cpu = measure_cpu_usage(interval, duration)
-        print(f"Average CPU usage over {duration} seconds: {average_cpu:.2f}%")
+        cpus = psutil.cpu_percent(interval=None, percpu=True)
+        ts = datetime.timestamp(datetime.now())
+        print(f"{ts}: {cpus}")
+        time.sleep(0.25)
 
 if __name__ == "__main__":
     main()
